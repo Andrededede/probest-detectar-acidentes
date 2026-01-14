ba354559db2d14da3ff037858432b18e28650964
André <andrelucas1708@gmail.com>
2026-01-14 18:01:41 -0300

    Remover geoLocation

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,8 +7,3 @@ export interface PredictionResponse {
   class: PredictionClass;
   confidence?: number; //Probabilidade de estar certo, se existir esse valor
 }
-
-export interface GeoLocation {
-  lat: number;
-  lon: number;
-}
